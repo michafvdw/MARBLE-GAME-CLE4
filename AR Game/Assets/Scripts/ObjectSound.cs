c3ca6b335d1a3ed33c55f3b721aa58c39434224b
Micha <74900539+michafvdw@users.noreply.github.com>
2021-06-09 14:12:51 +0200

    Script made for object sound. If object is spawned in it should play a sound. Commit by Micha

---
+++ AR Game/Assets/Scripts/ObjectSound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSound : MonoBehaviour
+{
+    AudioSource ac;
+         void Start (){
+               ac = GetComponent<AudioSource> ();
+           }
+}
+
+
