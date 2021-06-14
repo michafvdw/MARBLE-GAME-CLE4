ce0d9f2c40641f04d31033901cc28d8223ba47f8
Micha <74900539+michafvdw@users.noreply.github.com>
2021-06-14 10:20:42 +0200

    Code for level system has been added. PlayerController needs to be changed for AR in order for the code to work. Add the RightTarget code to the object that is the right target. If the ball hits the right target the game goes to the next level. - Micha

---
+++ AR Game/Assets/Scripts/RightTarget.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//if the target is correct, it moves on to a new level 
+//DOES NOT WORK YET: PLAYERCONTROLLER IS NOT YET DONE
+public class RightTarget : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController component = other.gameObject.GetComponent<PlayerController>();
+        if(component != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
